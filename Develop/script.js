364672541313d49f40b3c632f4fdb9f56b3b64c0
bluesman <rgsommer777@gmail.com>
2020-06-25 17:23:23 -0400

    Changed variable name for Valid Character length to 'ValidLength'

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Prompt user for password length and store as variable "PwLength"
-//var ValidPW = true
 
 //  Create a function "checkPW" to validate charater choice between 8 and 128
-
 function checkPW() {
-//  Prompt user to enter number of desired password characters
+//  Prompt user to enter number of desired password characters and store in variable "PwLength"
     var PwLength = prompt("Select number of characters for your password")
 /*
     Check to see if number of characters user entered is between 8 and 128;
     If true:
-        1. Set variable "ValidPw" to TRUE
-        2. Log message to console including state of "ValidPw" and valid password character count
+        1. Set variable "ValidLength" to TRUE
+        2. Log message to console including state of "ValidLength" and valid password character count
 */ 
-      if (PwLength >=8 && PwLength <=128) {
-        var ValidPwLength = true
-        console.log ("Valid Password Length? "+ValidPwLength)
+      if (PwLength >=8 && PwLength <=128)
+  {
+        var ValidLength = true
+        console.log ("Valid Password Length? "+ValidLength)
         console.log("Valid Password Length = "+ PwLength)
   }
 /*
     If password length criteria is not met:
-       1. Set "ValidPwLength" to FALSE
+       1. Set "ValidLength" to FALSE
        2. Log to console
        3. Throw a user alert to enter a number between 8 and 128 and 
        4. re-run the "PwLength" function (recursion)
 */
-      else {
-        var ValidPwLength = false  
-        console.log ("Valid Password Length? "+ValidPwLength)
+      else
+  { 
+        var ValidLength = false  
+        console.log ("Valid Password Length? "+ValidLength)
         alert ("Please choose a password character length between 8 and 128")
         checkPW()
-      
   }  
 }
 //Run the checkPW function  
