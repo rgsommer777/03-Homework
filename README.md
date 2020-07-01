b68433235e6a2bdd4fd26b1ba68f4408b1829725
bluesman <rgsommer777@gmail.com>
2020-06-30 23:27:44 -0400

    final files

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ AS AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
 SO THAT I can create a strong password that provides greater security
 
-
 ## My Approach
 1. Create function to set a valid password length
 2. Build a pool of candidate characters based on user input
@@ -14,7 +13,6 @@ SO THAT I can create a strong password that provides greater security
 4. Validate that the final password contains at least one data type for all the types the user selected.
     a. If all tests pass, display password to HTML page
     b. If any test fails, randomly substitute a character in the password for a valid character from the missing type, then display password to HTML page
-
 
 ## How it Works
 1. First created a prompt for user to enter number of desired password characters and store in variable "PwLength".
@@ -36,17 +34,20 @@ SO THAT I can create a strong password that provides greater security
 
 4. Randomly select "n" characters from the password pool, where "n" = the previously selected password length.
 
-5. 
+5. Test each data type selected for inclusion to ensure that they are represented. If they are not, a function called "randomSwapper" randomly substitutes a needed character type in the initial password until all teh criteria are met. 
 
 ## Files Included
+
 index.html
 contact.html
 portfolio.html
 
 ## What I Learned
+I learnd a lot about manipulating strings and how to randomize string elements. Most likely could have made it more efficient by utilizing arrays, but am still building my vocabulary there.
 
+(NOTE: I got everything working up until step 5. Still having some issues with the validation and so have commented most of this out for now. The "randomSwapper" is swapping in when it shouldn't so I was in progress of continuing to de-bug at time of submission).
 
 Deployed the app to my GitHub repository:
-<insert link>
-<insert link>
+GitHub link to project file repo: https://github.com/rgsommer777/03-Homework
+GitHub link to hosted page: https://rgsommer777.github.io/03-Homework/
   
--- script.js
+++ script.js
@@ -9,8 +9,6 @@ var password ="";
 var newpassword ="";
 
 
-
-
 // Target the "Generate Password" button via the DOM
 var generateBtn = document.querySelector("#generate");
 //Add event listener to "Generate Password" button -
@@ -50,7 +48,6 @@ document.querySelector ("textarea").placeholder = "";
   }
 
 //Function "buildPwPool" builds the list of possible password characters based on user selections:
-
 function buildPwPool()
 {
     isLC = confirm ("Should your password contain lower case letters?");
@@ -86,87 +83,78 @@ function buildPwPool()
       alert ("Please select at least one (1) character type")
 
       buildPwPool()
-
     }
-    // if (isAny = 0) {
-    // console.log (isAny);
-    // }
  }  
  buildPwPool()
 
-    
-//Generate the password randomly 
+//Randomize the password candidate pool
 let password ="";
 
  for (var i=0; i<= pwLength-1; i++){
  password = password + pwPool.charAt(Math.floor(Math.random() * Math.floor(pwPool.length -1)));
  }
- console.log(password)
+ console.log("initial password: "+password)
   
 
 // Return the first characters of the random password (up to the chosen password length)
 pwPool.slice(0,pwLength)
 
-  
-
-    // var LCArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    // var UCArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    // var NumArray = ['0','1','2','3','4','5','6','7','8','9']
-    //var SpCArray = ['!','@','#','$','%','^','&','*','(',')','_','+','`','-','=','?','<','>','"'] 
-    
-// }
-    // Setup
-    //console.log("LCArray: " + LCArray.join(","));
-    
-    // Test password for inclusion of lower case alphabet
-      // var patt = RegExp("[0-9]");
-      // res = patt.test(password);
-     
-    // Test password for inclusion of lower case alphabet  
-    //Create a function to swap in a random target character from source data type to replace target data type 
-    
-      var re = new RegExp('[0-9]');
-      if (re.test(password)) {
-        console.log("Valid for numbers");
-      } else {
-        console.log("Invalid for numbers");
-      } 
-      var re = new RegExp('[A-Z]');
-      if (re.test(password)) {
-        console.log("Valid for upper case");
-      } else {
-        console.log("Invalid for upper case");
+// Test password for inclusion of:
+    // numbers...  
+    //     var re = new RegExp('[0-9]');
+    //   if (re.test(password)) {
+    //     console.log("Valid for numbers");
+    //   } else {
+    //     console.log("Invalid for numbers");
+    //   } 
+    // // ...Upper case alphabet...  
+    //     var re = new RegExp('[A-Z]');
+    //   if (re.test(password)) {
+    //     console.log("Valid for upper case");
+    //   } else {
+    //     console.log("Invalid for upper case");
+    //   }
+    // // ...Lower case alphabet...  
+    //   var re = new RegExp('[a-z]');
+    //   if (re.test(password)) {
+    //     console.log("Valid for lower case");
+    //   } else {
+    //     console.log("Invalid for lower case");
+    //   }
+    // // ...and special characters   
+    //   var re = new RegExp('[!,@,#,$,%,^,&,*,(,),_,+,`,-,=,?,<,>,]');
+    //   if (re.test(password)) {
+    //     console.log("Valid for special characters");
+    //   } else {
+    //     console.log("Invalid for special characters");
+    //   }
+    // Test the data types that were selected for inclusion   
+      if (isNum=true) {
+      test4Num()    
       }
-      var re = new RegExp('[a-z]');
-      if (re.test(password)) {
-        console.log("Valid for lower case");
-      } else {
-        console.log("Invalid for lower case");
+      else{
+        return;
       }
-      var re = new RegExp('[!,@,#,$,%,^,&,*,(,),_,+,`,-,=,?,<,>,]');
-      if (re.test(password)) {
-        console.log("Valid for special characters");
-      } else {
-        console.log("Invalid for special characters");
+      if (isUC=true) {
+      test4UC()    
       }
-    
-    if (isNum=true) {
-    test4Num()  
-    
-    
-    function randomSwapper(password,Source) {
-      pwRandNum = Math.floor((Math.random() * password.length))+1; //Computes a random number between 1 and source string length
-      pwSlicer = pwRandNum -1 ;
-      pwSlice = password.replace(pwSlicer,pwRandNum);
-      pwRandChr = password.charAt(pwSlicer);
-      srcRandNum = Math.floor((Math.random() * Source.length))+1; //Computes a random number between 1 and source string length
-      srcSlicer = srcRandNum -1;
-      srcSlice = Source.replace(srcSlicer,srcRandNum);
-      srcRandChr = Source.charAt(srcSlicer);
-      newpw = password.replace(pwRandChr,srcRandChr);
+      else{
+        return;
+      }
       
-     
-      }
+      // if (isLC=true) {
+      // test4LC()    
+      // }
+      
+      // if (isSpC=true) {
+      // test4SpC()    
+      // }
+    //Create functions to test the selected data type is actually included in the password
+    //If it is not present, but supposed to be there, call teh function "randomSwapper" to substitute
+    //a valid character to fulfill the condition 
+        
+    //first for numbers...
+  
     function test4Num() {  
         var re = new RegExp('[0-9]');
         if (re.test(password)) {
@@ -178,16 +166,68 @@ pwPool.slice(0,pwLength)
           
           test4Num()
         }
-   // testPwValidity()
-        document.querySelector ("textarea").placeholder = password;    
-  //document.querySelector ("textarea").placeholder = password;    
-    }
- 
-    // var re = new RegExp('[0-9]');
-    // if (re.test(password)) {
-    //   console.log("Valid for numbers");
-    // } else {
-    //   console.log("Invalid for numbers");
-  }
+          console.log("new password: "+password);
+          document.querySelector ("textarea").placeholder = password;
+      }   
+       
+//...then for upper case alphabet...
+      
+    function test4UC() {  
+        var re = new RegExp('[A-Z]');
+        if (re.test(password)) {
+          console.log("Valid for upper case");
+        } else {
+          console.log("Invalid for upper case");
+          randomSwapper(password,UC);
+          password = newpw;
+                
+          test4UC()
+        }
+         console.log("new password: "+password);
+         document.querySelector ("textarea").placeholder = password;
+      }
+              //...then for lower case alphabet...
   
-}
+  //function test4LC() {  
+  //    var re = new RegExp('[a-z]');
+  //    if (re.test(password)) {
+  //      console.log("Valid for lower case");
+  //    } else {
+  //      console.log("Invalid for lower case");
+  //      randomSwapper(password,LC);
+  //      password = newpw;
+                        
+  //      test4LC()
+  //    }
+  //     console.log("new password: "+password);
+  //     document.querySelector ("textarea").placeholder = password;
+  //  }
+  //function test4SpC() {  
+  //    var re = new RegExp('[!,@,#,$,%,^,&,*,(,),_,+,`,-,=,?,<,>,]');
+  //    if (re.test(password)) {
+  //      console.log("Valid for special characters");
+  //    } else {
+  //      console.log("Invalid for special characters");
+  //      randomSwapper(password,SpC);
+  //      password = newpw;
+                          
+  //      test4SpC()
+  //    }
+  //     console.log("new password: "+password);
+     //  document.querySelector ("textarea").placeholder = password;
+    //}
+ //Create a function to swap in a random target character from source data type to replace target data type 
+   function randomSwapper(password,Source) {
+    pwRandNum = Math.floor((Math.random() * password.length))+1; //Computes a random number between 1 and source string length
+    pwSlicer = pwRandNum -1 ;
+    pwSlice = password.replace(pwSlicer,pwRandNum);
+    pwRandChr = password.charAt(pwSlicer);
+    srcRandNum = Math.floor((Math.random() * Source.length))+1; //Computes a random number between 1 and source string length
+    srcSlicer = srcRandNum -1;
+    srcSlice = Source.replace(srcSlicer,srcRandNum);
+    srcRandChr = Source.charAt(srcSlicer);
+    newpw = password.replace(pwRandChr,srcRandChr);
+  
+    
+   }   
+}  
